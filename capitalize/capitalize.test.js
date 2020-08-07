599adbafa64e3ca27e35381b07c835cc93be723c
Angelos Bousis <bousis.ang@gmail.com>
2020-08-07 10:15:43 +0300

    Added more test cases.

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,4 +1,16 @@
 let capitalize = require('./capitalize');
-test('first letter is capital', () => {
+test('first letter is lowercase', () => {
     expect(capitalize('testdrivendev')).toBe('Testdrivendev');    
+});
+
+test('first letter is uppercase', () => {
+    expect(capitalize('Testdrivendev')).toBe('Testdrivendev');
+});
+
+test('works with number as first letter', () => {
+    expect(capitalize('0testdrivendev')).toBe('0testdrivendev');
+});
+
+test('works with punctuation', () => {
+    expect(capitalize('!testdrivendev')).toBe('!testdrivendev');
 });
